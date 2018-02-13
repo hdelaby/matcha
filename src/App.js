feb6d92a64ea18ebf36b7547feb01d2f7ef1319f
Hugo DELABY <hdelaby@e3r4p3.42.fr>
2018-02-13 15:11:56 +0100

    Added redux and trying to understand

--- src/App.js
+++ src/App.js
@@ -1,21 +1,112 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, PropTypes } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import { connect } from 'react-redux';
 import './App.css';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>Home</h2>
       </div>
+    )
+  };
+}
+
+class SignIn extends Component {
+  render() {
+    return (
+      <form>
+        Username: 
+        <input type="text" name="username"/>
+        <br />
+        Email: 
+        <input type="text" name="email"/>
+        <br />
+        First name: 
+        <input type="text" name="firstname"/>
+        <br />
+        Surname: 
+        <input type="text" name="surname"/>
+        <br />
+        Password: 
+        <input type="text" name="password"/>
+        <br />
+      </form>
+    )
+  }
+}
+
+class LogIn extends Component {
+  render() {
+    return (
+      <form>
+        Username: 
+        <input type="text" name="username"/>
+        <br />
+        Password: 
+        <input type="text" name="password"/>
+        <br />
+      </form>
     );
   }
 }
 
-export default App;
+class Profile extends Component {
+  render() {
+    return (
+      <h2>This will be the user profile for user {this.props.match.params.id}!</h2>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/signin">Sign in</Link></li>
+            <li><Link to="/login">Log in</Link></li>
+            <li><Link to="/profile/36">Profile</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/signin" component={SignIn}/>
+          <Route path="/login" component={LogIn}/>
+          <Route path="/profile/:id" component={Profile}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  quote: PropTypes.string,
+  isAuthenticated: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string,
+  isSecretQuote: PropTypes.bool.isRequired
+}
+
+function mapStateToProps(state) {
+  const { quotes, auth } = state
+  const { quote, authenticated } = quotes
+  const { isAuthenticated, errorMessage } = auth
+
+  return {
+    quote,
+    isSecretQuote: authenticated,
+    isAuthenticated,
+    errorMessage
+  }
+}
+
+export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import api from './middleware/api'
+import quotesApp from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let createStoreWithMiddleware = applyMiddleware(thunkMiddleware, api)(createStore);
+
+let store = createStoreWithMiddleware(quotesApp);
+
+ReactDOM.render((
+<Provider store={store}>
+  <App />
+</Provider>
+), document.getElementById('root'));
+
 registerServiceWorker();
