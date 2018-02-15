de4e8b17454a4bc2b604e7e21c8c9684b4d8ad3f
Hugo DELABY <hdelaby@e2r7p6.42.fr>
2018-02-15 17:45:42 +0100

    WIP async

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,64 @@
-let nextTodoId = 0
+import fetch from 'cross-fetch'
 
-export const ADD_TODO = 'ADD_TODO';
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
 
-export const addTodo = text => {
+export function selectSubreddit(subreddit) {
   return {
-    type: ADD_TODO,
-    id: nextTodoId++,
-    text
+    type: SELECT_SUBREDDIT,
+    subreddit
   }
 }
 
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
-
-export const setVisibilityFilter = filter => {
+export function invalidateSubreddit(subreddit) {
   return {
-    type: SET_VISIBILITY_FILTER,
-    filter
+    type: INVALIDATE_SUBREDDIT,
+    subreddit
   }
 }
 
-export const TOGGLE_TODO = 'TOGGLE_TODO';
-
-export const toggleTodo = id => {
+export function requestPosts(subreddit) {
   return {
-    type: TOGGLE_TODO,
-    id
+    type: REQUEST_POSTS,
+    subreddit
+  }
+}
+
+export function receivePosts(subreddit, json) {
+  return {
+    type: RECEIVE_POSTS,
+    subreddit,
+    posts: json.data.children.map(child => child.data),
+    receivedAt: Date.now()
+  }
+}
+
+function fetchPosts(subreddit) {
+  return dispatch => {
+    dispatch(requestPosts(subreddit))
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then(res => res.json())
+      .then(json => dispatch(receivePosts(subreddit, json)))
+  }
+}
+
+function shouldFetchPosts(state, subreddit) {
+  const posts = state.postsBySubreddit[subreddit]
+
+  if (!posts) {
+    return true
+  } else if (posts.isFetching) {
+    return false
+  }
+  return posts.didInvalidate
+}
+
+export function fetchPostsIfNeeded(subreddit) {
+  return (dispatch, getState) => {
+    if (shouldFetchPosts(getState(), subreddit)) {
+      return dispatch(fetchPosts(subreddit))
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
+import 'babel-polyfill'
+
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import './index.css';
-import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
-import todoApp from './reducers'
+import Root from 'containers/Root'
 
-let store =  createStore(todoApp);
-
-// let createStoreWithMiddleware = applyMiddleware(thunkMiddleware, api)(createStore);
-
-// let store = createStoreWithMiddleware(quotesApp);
-
-render((
-  <Provider store={store}>
-    <App />
-  </Provider>
-), document.getElementById('root'));
+render(<Root />, document.getElementById('root'));
 
 // registerServiceWorker();
